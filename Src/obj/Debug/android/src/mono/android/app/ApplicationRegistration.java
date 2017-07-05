e440f16a8e87b839fe2db8e9daaca388a9428e9d
Daniel <Daeynasvistas@gmail.com>
2017-07-05 14:16:32 +0100

    Commit update

---
+++ Src/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("com.xamarin.beta_3.MainApplication, Beta_3, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md50ca5b6ddd238534adbed8fc0927af193.MainApplication.class, md50ca5b6ddd238534adbed8fc0927af193.MainApplication.__md_methods);
+		
+	}
+}
